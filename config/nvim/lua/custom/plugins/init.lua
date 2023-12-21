db09a2683bb309a67cc4c659e17b4619d3121373
João Paulo Dubas <joao.dubas@noreply.gitea.dubas.dev>
2023-12-21 14:06:53 +0000

    feat(nvim): add oil.nvim file explorer (#20)
    Add [oil.nvim][0] file explorer to make it easier to handle folders and files.
    
    [0]: https://github.com/stevearc/oil.nvim
    
    Reviewed-on: https://gitea.dubas.dev/joao.dubas/ide/pulls/20

--- config/nvim/lua/custom/plugins/init.lua
+++ config/nvim/lua/custom/plugins/init.lua
@@ -96,10 +96,10 @@ return {
           local opts = { buffer = bufnr }
           vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
           vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-          vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-          vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-          vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-          vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+          vim.keymap.set('t', '<C-h>', [[<cmd>wincmd h<cr>]], opts)
+          vim.keymap.set('t', '<C-j>', [[<cmd>wincmd j<cr>]], opts)
+          vim.keymap.set('t', '<C-k>', [[<cmd>wincmd k<cr>]], opts)
+          vim.keymap.set('t', '<C-l>', [[<cmd>wincmd l<cr>]], opts)
           vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
         end
       })
@@ -109,5 +109,13 @@ return {
         { '<leader>mc', '<cmd>ToggleTermSendCurrentLine<cr>', desc = 'Send [c]urrent line under the cursor', noremap = true }
       }
     end
-  }
+  },
+  {
+    'stevearc/oil.nvim',
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { '-', '<cmd>Oil<cr>', desc = "Open parent directory" },
+    },
+    opts = {},
+  },
 }
